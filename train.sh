9c3b3bbdc6864140297054484b39a3410b92e453
Nuno Dantas <nuno.dantas97@gmail.com>
2023-11-01 11:50:40 +0100

    training configs

--- train.sh
+++ train.sh
@@ -1,11 +1,5 @@
 eval "$(conda shell.bash hook)"
-sudo rm -r data
 conda activate /mnt/6d4d3819-c0f1-4f9d-b056-08610dac2519/CondaEnvs/habitat2
-python -m habitat_sim.utils.datasets_download --uids habitat_test_scenes --data-path data/
-python -m habitat_sim.utils.datasets_download --uids habitat_test_pointnav_dataset --data-path data/
-#python -m habitat_sim.utils.datasets_download --uids mp3d_example_scene --data-path data/
-unset HABITAT_ENV_DEBUG
-python -u -m habitat_baselines.run --config-name=pointnav/ppo_pointnav_example.yaml
-#export HABITAT_ENV_DEBUG=1
-#python -u -m habitat_baselines.run --config-name=pointnav/ppo_pointnav_unreal.yaml
-
+sudo rm -r data/new_checkpoints
+mkdir data/new_checkpoints
+python -u -m habitat_baselines.run   --config-name=pointnav/ppo_pointnav_unreal.yaml
