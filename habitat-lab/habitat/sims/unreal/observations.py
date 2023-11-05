0597d981af373373f2cbf36c4bfa180a7396561f
Nuno <nuno.dantas97@gmail.com>
2023-11-05 22:27:46 +0100

    fix typo 2

--- habitat-lab/habitat/sims/unreal/observations.py
+++ habitat-lab/habitat/sims/unreal/observations.py
@@ -114,7 +114,7 @@ class ObservationsSingleton(metaclass=Singleton):
             elif DISPLAY_DEPTH and "Depth" in key:
                 display_rgb(image)
 
-            if self.buffers[key]:
+            if self.buffers["_previous_step_reset"]:
                 print(f"Previous action resulted in a reset because of: {self.buffers['_previous_step_reset_reason']}")
 
         # print(f"Got observations: {', '.join([k for k in obj.keys()])}")
